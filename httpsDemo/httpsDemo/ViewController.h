ea3418f8743211242e81de9ae2e85d12b5de90c8
caoqin <caoqinql@163.com>
2016-12-21 08:50:19 +0800

    httpsDemo

---
+++ httpsDemo/httpsDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  httpsDemo
+//
+//  Created by runo on 16/11/22.
+//  Copyright © 2016年 com.runo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ httpsDemo/httpsDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  httpsDemo
+//
+//  Created by runo on 16/11/22.
+//  Copyright © 2016年 com.runo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
